4dc9a286d728fd369ce0d720ffa0da507c68bb14
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-07-16 16:56:31 +0400

    Setup project
    - Remove storyboards
    - Create folders hierarchy
    - Setup target

---
+++ vk-internship-assignment/vk-internship-assignment/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  vk-internship-assignment
+//
+//  Created by Artem Tebenkov on 16.07.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
