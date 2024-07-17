bf418d5869d5790c32068e302c93771d431ac886
Artem Tebenkov <artem.tebenkov@icloud.com>
2024-07-17 14:36:45 +0400

    Refactor slider cell

--- vk-internship-assignment/vk-internship-assignment/Modules/Main/View/WeatherSlider/Cell/WeatherSliderCell.swift
+++ vk-internship-assignment/vk-internship-assignment/Modules/Main/View/WeatherSlider/Cell/WeatherSliderCell.swift
@@ -11,9 +11,23 @@ final class WeatherSliderCell: UICollectionViewCell {
 
     // MARK: - Subviews
 
+    private let blurBackgroundView: UIView = {
+        let view = UIView()
+        view.layer.cornerRadius = UIConstants.BackgroundView.cornerRadius
+        view.clipsToBounds = true
+        view.transform = UIConstants.BackgroundView.initialTransform
+        view.layer.opacity = 0
+        return view
+    }()
+
+    private let blurEffectView: UIVisualEffectView = {
+        let blurEffect = UIBlurEffect(style: .systemThinMaterial)
+        let view = UIVisualEffectView(effect: blurEffect)
+        return view
+    }()
+
     private let label: UILabel = {
         let label = UILabel()
-
         return label
     }()
 
@@ -34,40 +48,65 @@ final class WeatherSliderCell: UICollectionViewCell {
     func setupWith(_ string: String) {
         label.text = string
     }
+
+    func showWithAnimation() {
+        UIView.animate(
+            withDuration: UIConstants.Animation.duration,
+            delay: UIConstants.Animation.delay
+        ) {
+            self.blurBackgroundView.transform = .identity
+            self.blurBackgroundView.layer.opacity = 1
+        }
+    }
+
+    func hide() {
+        blurBackgroundView.transform = UIConstants.BackgroundView.initialTransform
+        blurBackgroundView.layer.opacity = 0
+    }
 }
 
 // MARK: - Setup subviews
 
 private extension WeatherSliderCell {
     func setupSubviews() {
-        contentView.addSubviews([label])
+        contentView.addSubviews([blurBackgroundView])
+        blurBackgroundView.addSubviews([blurEffectView, label])
     }
 
     func setupLayout() {
         NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: contentView.topAnchor),
-            label.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+            blurBackgroundView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            blurBackgroundView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            blurBackgroundView.widthAnchor.constraint(
+                equalTo: contentView.widthAnchor,
+                multiplier: UIConstants.BackgroundView.widthMultipier),
+            blurBackgroundView.heightAnchor.constraint(
+                equalTo: contentView.widthAnchor,
+                multiplier: UIConstants.BackgroundView.widthMultipier),
+
+            blurEffectView.topAnchor.constraint(equalTo: blurBackgroundView.topAnchor),
+            blurEffectView.leadingAnchor.constraint(equalTo: blurBackgroundView.leadingAnchor),
+            blurEffectView.trailingAnchor.constraint(equalTo: blurBackgroundView.trailingAnchor),
+            blurEffectView.bottomAnchor.constraint(equalTo: blurBackgroundView.bottomAnchor),
+
+            label.centerXAnchor.constraint(equalTo: blurBackgroundView.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: blurBackgroundView.centerYAnchor)
         ])
     }
 }
 
 // MARK: - UI constants
 
-private enum Spec {
-    enum TableView {
-        static let contentOffset: UIEdgeInsets = .init(top: 16, left: 0, bottom: 0, right: 0)
-        static let tableViewCellHeight: CGFloat = 84
-        static let numberOfShimmerCells: Int = 9
+private enum UIConstants {
+    enum Animation {
+        static let duration: TimeInterval = 0.4
+        static let delay: TimeInterval = 0.35
     }
 
-    enum Cell {
-        static let height: CGFloat = 84
-        static let amount: Int = 9
-    }
-
-    enum EmptyStateView {
-        static let top: CGFloat = 80
+    enum BackgroundView {
+        static let initialTransform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+        static let cornerRadius: CGFloat = 16
+        static let widthMultipier: CGFloat = 0.7
+        static let heightMultipier: CGFloat = 0.5
     }
 }
